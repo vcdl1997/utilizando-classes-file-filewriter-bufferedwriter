acea3a90ff222d10c6c7f84de83761306636ce97
Vinicius Costa <vcdl1997@gmail.com>
2021-08-01 02:03:22 -0300

    Usando as classes: BufferedReader e FileReader para a leitura de arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,29 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		File file = new File("C:\\temp\\in.txt");
-		Scanner sc = null;
-
-		try {
-			sc = new Scanner(file);
+		String path = "C:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 			
-		}catch(IOException e) {
+		}catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}finally {
-			if(sc != null) sc.close();
 		}
+		
 	}
 
 }
