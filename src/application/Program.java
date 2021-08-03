43c3fbd6ca8daadb4346e805340883a2f64615f3
Vinicius Costa <vcdl1997@gmail.com>
2021-08-03 19:51:16 -0300

    Exercicio proposto de Java com a utilização das classes: File, FileWriter e BufferedWriter para manipulação de arquivos e pastas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,52 @@
 package application;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		File spreadSheet = new File("C:\\temp\\planilhas\\products.csv");
+		File[] folders = new File("C:\\temp\\planilhas").listFiles(File::isDirectory);
+		Boolean folderIsExists = Arrays.asList(folders).stream().anyMatch(x -> x.getName().equals("out"));	
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date d1 = sdf.parse("30/07/2021");
-		Date d2 = sdf.parse("01/08/2021");
-		Long diff = d2.getTime() - d1.getTime();
+		if(!folderIsExists) {
+			Boolean created = new File("C:\\temp\\planilhas\\out").mkdir();
+			if(created) System.out.println("Directory successfully created");
+		}
+			
 		
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeInMillis(diff);
+		try (
+			Scanner scan = new Scanner(spreadSheet); 
+			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\temp\\planilhas\\out\\summary.csv", true));
+		){
+			
+			while(scan.hasNextLine()) {
+				String[] parametros = scan.nextLine().split(",");
+				
+				String name = parametros[0];
+				Double price = Double.valueOf(parametros[1]);
+				Integer quantity = Integer.valueOf(parametros[2]);
+				Product product = new Product(name, price, quantity);
+				
+				bw.write(product.getName() + "," + product.getAccountTotal());
+				bw.newLine();
+			}
+			
+			System.out.println("Spreadsheet created successfully");
+			
+			scan.close();
+		}catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 		
-		System.out.println(calendar.get(Calendar.DAY_OF_MONTH)); //Diferença em dias
 	}
 }
